f9f24d72f6f434839fbed96cb4e564edcb428433
Giannina <giannina@Mac-mini-de-Dot175.local>
2015-09-07 12:28:46 -0300

    Agrego maquetado de log in con 2 cols

--- login-2col.php
+++ login-2col.php
@@ -4,10 +4,10 @@
 	<div class="base">
 
 		<!--contenido-->
-		<div id="contenidoGeneral" class="contenido contenidoGeneral col-xs-12 col-sm-12 col-md-12 ol-lg-12">
+		<div id="contenidoGeneral" class="contenido contenidoGeneral col-xs-12">
 
 			<!--TITULO columna 1-->
-			<div id="titulo-col1" class="columnasTitulo col-xs-12 col-sm-4 col-md-4 ol-lg-4">
+			<div id="titulo-col1" class="columnasTitulo col-xs-12 col-sm-6">
 				<div class="titulo activo">
 					<h3 class="text-uppercase">Marketing<br><span>Net</span></h3>
 					<a href="#" >
@@ -33,7 +33,7 @@
 			<!--end / TITULO columna 1-->
 
 			<!--TITULO columna 2-->
-			<div id="titulo-col2" class=" columnasTitulo col-xs-12 col-sm-4 col-md-4 ol-lg-4">
+			<div id="titulo-col2" class=" columnasTitulo col-xs-12 col-sm-6">
 				<div class="titulo activo">
 					<h3 class="text-uppercase">Vendedor<br><span>Estrella</span></h3>
 					<a href="#" >
@@ -59,46 +59,14 @@
 			</div>
 			<!--end / TITULO columna 2-->
 
-			<!--TITULO columna 3-->
-			<div id="titulo-col3" class=" columnasTitulo col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-				<div class="titulo activo">
-					<h3 class="text-uppercase">Plan<br><span class='de'> de </span><span>Negocios</span></h3>
-					<a href="#" >
-						<div class="block-icon" >
-							<img class="imagen" src="assets/images/flecha.png" alt="">
-							<p class="text text-uppercase">Ver estado</p>
-						</div>
-					</a>
-					<a href="#" >
-						<div class="block-icon link-mostrar" to="3">
-							<img class="imagen" src="assets/images/mas.png" alt="">
-							<p class=" text text-uppercase">mas</p>
-						</div>
-					</a>
-					<a href="#" >
-						<div class="block-icon link-cerrar  oculto" to="3">
-							<img class="imagen" src="assets/images/cerrar.png" alt="">
-							<p class="text text-uppercase" >cerrar</p>
-						</div>
-					</a>
-				</div>
-			</div>
-			<!--end / TITULO columna 3-->
-
-
-
-
-
-
-
 			<!--INFO columna 1-->
 			<div id='info-col1' class="columnasInfo oculto">
-				<div class="item item-A  col-xs-12 col-sm-4 col-md-4 ol-lg-4">
+				<div class="item item-A  col-xs-12 col-sm-4">
 					<p class="text ">1) Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.</p>
 					<p class="text ">El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.</p>
 				</div>
 
-				<div class="item item-B col-xs-12 col-sm-4 col-md-4 ol-lg-4 ">
+				<div class="item item-B col-xs-12 col-sm-4">
 					<p class="text destacado text-uppercase">ARTÍCULOS PROMOCIONALES CON IDENTIFICACIÓN DE MARCA.</p>
 					<p class="text block">La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país. </p>
 					<p class="text ">La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.</p>
@@ -115,7 +83,7 @@
 					</ul>
 				</div>
 
-				<div class="item item-C col-xs-12 col-sm-4 col-md-4 ol-lg-4">
+				<div class="item item-C col-xs-12 col-sm-4">
 					<img class="imagen" src="assets/images/imagen-A.png" alt="">
 				</div>
 
@@ -124,12 +92,12 @@
 
 			<!--INFO columna 2-->
 			<div id='info-col2' class="columnasInfo oculto">
-				<div class="item item-A  col-xs-12 col-sm-4 col-md-4 ol-lg-4">
+				<div class="item item-A  col-xs-12 col-sm-4">
 					<p class="text ">2) Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.</p>
 					<p class="text ">El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.</p>
 				</div>
 
-				<div class="item item-B col-xs-12 col-sm-4 col-md-4 ol-lg-4 ">
+				<div class="item item-B col-xs-12 col-sm-4">
 					<p class="text destacado text-uppercase">ARTÍCULOS PROMOCIONALES CON IDENTIFICACIÓN DE MARCA.</p>
 					<p class="text block">La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país. </p>
 					<p class="text ">La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.</p>
@@ -146,54 +114,22 @@
 					</ul>
 				</div>
 
-				<div class="item item-C col-xs-12 col-sm-4 col-md-4 ol-lg-4">
+				<div class="item item-C col-xs-12 col-sm-4">
 					<img class="imagen" src="assets/images/imagen-A.png" alt="">
 				</div>
 
 			</div>
 			<!--end / INFO columna 2 -->
 
-			<!--INFO columna 3-->
-			<div id='info-col3' class="columnasInfo oculto">
-				<div class="item item-A  col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-					<p class="text ">3) Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.</p>
-					<p class="text ">El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.</p>
-				</div>
-
-				<div class="item item-B col-xs-12 col-sm-4 col-md-4 ol-lg-4 ">
-					<p class="text destacado text-uppercase">ARTÍCULOS PROMOCIONALES CON IDENTIFICACIÓN DE MARCA.</p>
-					<p class="text block">La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país. </p>
-					<p class="text ">La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.</p>
-					<p class="text block">Este programa te permitirá tener material promocional con:</p>
-					<ul class="block">
-						<li class="text-uppercase">
-							<span class="glyphicon glyphicon-chevron-right"></span>
-							El logo de tu empresa
-						</li>
-						<li class="text-uppercase">
-							<span class="glyphicon glyphicon-chevron-right"></span>
-							El logo de nufarm
-						</li>
-					</ul>
-				</div>
-
-				<div class="item item-C col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-					<img class="imagen" src="assets/images/imagen-A.png" alt="">
-				</div>
-
-			</div>
-			<!--end / INFO columna 3 -->
-
-
 			<!--FOOTER-->
-			<div class="footer-menu col-xs-12 col-sm-12 col-md-12 ol-lg-12">
+			<div class="footer-menu col-xs-12">
 				<div class="block-items">
 					<a href="#" >
 						<div class="item ">
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
+							<div class="col-xs-12">
 								<img class="imagen" src="assets/images/ICON-A.png" alt="">
 							</div>
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
+							<div class="col-xs-12">
 								<img class="flecha" src="assets/images/sub-flecha.png" alt="">
 							</div>
 							<hr>		
@@ -203,27 +139,14 @@
 
 					<a href="#" >
 						<div class="item">
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
+							<div class="col-xs-12">
 								<img class="imagen" src="assets/images/ICON-B.png" alt="">
 							</div>
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
+							<div class="col-xs-12">
 								<img class="flecha" src="assets/images/sub-flecha.png" alt="">
 							</div>
 							<hr>		
 							<p class="text-uppercase">Blog</p>
-						</div>
-					</a>
-
-					<a href="#" >
-						<div class="item">
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
-								<img class="imagen" src="assets/images/ICON-C.png" alt="">
-							</div>
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
-								<img class="flecha" src="assets/images/sub-flecha.png" alt="">
-							</div>
-							<hr>		
-							<p class="text-uppercase">Contacto</p>
 						</div>
 					</a>
 				</div>
