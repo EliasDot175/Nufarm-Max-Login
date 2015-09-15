2e0533cab39dd793f0f3dfc05a1e8fa11b44ae0b
EliasDot175 <elias@dot175.com>
2015-09-15 12:16:26 -0300

    Primera instancia de maquetado del log in 3 cols

--- login.php
+++ login.php
@@ -1,243 +1,201 @@
 <?php include 'inc/header.php' ?>
 
-	<!--base-->
-	<div class="base">
-
-		<!--contenido-->
-		<div id="contenidoGeneral" class="contenido contenidoGeneral col-xs-12 col-sm-12 col-md-12 ol-lg-12">
-
-			<!--TITULO columna 1-->
-			<div id="titulo-col1" class="columnasTitulo col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-				<div class="titulo activo">
-					<h3 class="text-uppercase">Marketing<br><span>Net</span></h3>
-					<a href="#" >
-						<div class="block-icon">
-							<img class="imagen" src="assets/images/flecha.png" alt="">
-							<p class="text text-uppercase">Ingresar a catálogo</p>
-						</div>
-					</a>
-					<a href="#" >
-						<div class="block-icon link-mostrar" to="1">
-							<img class="imagen" src="assets/images/mas.png" alt="">
-							<p class=" text text-uppercase">mas</p>
-						</div>
-					</a>
-					<a href="#" >
-						<div class="block-icon link-cerrar  oculto" to="1">
-							<img class="imagen" src="assets/images/cerrar.png" alt="">
-							<p class="text text-uppercase" >cerrar</p>
-						</div>
-					</a>
-				</div>
-			</div>
-			<!--end / TITULO columna 1-->
-
-			<!--TITULO columna 2-->
-			<div id="titulo-col2" class=" columnasTitulo col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-				<div class="titulo activo">
-					<h3 class="text-uppercase">Vendedor<br><span>Estrella</span></h3>
-					<a href="#" >
-						<div class="block-icon">
-							<img class="imagen" src="assets/images/flecha.png" alt="">
-							<p class="text text-uppercase">Ver estado</p>
-						</div>
-					</a>
-					<a href="#" >
-						<div class="block-icon link-mostrar" to="2">
-							<img class="imagen" src="assets/images/mas.png" alt="">
-							<p class=" text text-uppercase">mas</p>
-						</div>
-					</a>
-					<a href="#" >
-						<div class="block-icon link-cerrar  oculto" to="2">
-							<img class="imagen" src="assets/images/cerrar.png" alt="">
-							<p class="text text-uppercase" >cerrar</p>
-						</div>
-					</a>
-
-				</div>
-			</div>
-			<!--end / TITULO columna 2-->
-
-			<!--TITULO columna 3-->
-			<div id="titulo-col3" class=" columnasTitulo col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-				<div class="titulo activo">
-					<h3 class="text-uppercase">Plan<br><span class='de'> de </span><span>Negocios</span></h3>
-					<a href="#" >
-						<div class="block-icon" >
-							<img class="imagen" src="assets/images/flecha.png" alt="">
-							<p class="text text-uppercase">Ver estado</p>
-						</div>
-					</a>
-					<a href="#" >
-						<div class="block-icon link-mostrar" to="3">
-							<img class="imagen" src="assets/images/mas.png" alt="">
-							<p class=" text text-uppercase">mas</p>
-						</div>
-					</a>
-					<a href="#" >
-						<div class="block-icon link-cerrar  oculto" to="3">
-							<img class="imagen" src="assets/images/cerrar.png" alt="">
-							<p class="text text-uppercase" >cerrar</p>
-						</div>
-					</a>
-				</div>
-			</div>
-			<!--end / TITULO columna 3-->
-
-
-
-
-
-
-
-			<!--INFO columna 1-->
-			<div id='info-col1' class="columnasInfo oculto">
-				<div class="item item-A  col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-					<p class="text ">1) Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.</p>
-					<p class="text ">El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.</p>
-				</div>
-
-				<div class="item item-B col-xs-12 col-sm-4 col-md-4 ol-lg-4 ">
-					<p class="text destacado text-uppercase">ARTÍCULOS PROMOCIONALES CON IDENTIFICACIÓN DE MARCA.</p>
-					<p class="text block">La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país. </p>
-					<p class="text ">La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.</p>
-					<p class="text block">Este programa te permitirá tener material promocional con:</p>
-					<ul class="block">
-						<li class="text-uppercase">
-							<span class="glyphicon glyphicon-chevron-right"></span>
-							El logo de tu empresa
-						</li>
-						<li class="text-uppercase">
-							<span class="glyphicon glyphicon-chevron-right"></span>
-							El logo de nufarm
-						</li>
-					</ul>
-				</div>
-
-				<div class="item item-C col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-					<img class="imagen" src="assets/images/imagen-A.png" alt="">
-				</div>
-
-			</div>
-			<!--end / INFO columna 1 -->
-
-			<!--INFO columna 2-->
-			<div id='info-col2' class="columnasInfo oculto">
-				<div class="item item-A  col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-					<p class="text ">2) Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.</p>
-					<p class="text ">El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.</p>
-				</div>
-
-				<div class="item item-B col-xs-12 col-sm-4 col-md-4 ol-lg-4 ">
-					<p class="text destacado text-uppercase">ARTÍCULOS PROMOCIONALES CON IDENTIFICACIÓN DE MARCA.</p>
-					<p class="text block">La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país. </p>
-					<p class="text ">La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.</p>
-					<p class="text block">Este programa te permitirá tener material promocional con:</p>
-					<ul class="block">
-						<li class="text-uppercase">
-							<span class="glyphicon glyphicon-chevron-right"></span>
-							El logo de tu empresa
-						</li>
-						<li class="text-uppercase">
-							<span class="glyphicon glyphicon-chevron-right"></span>
-							El logo de nufarm
-						</li>
-					</ul>
-				</div>
-
-				<div class="item item-C col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-					<img class="imagen" src="assets/images/imagen-A.png" alt="">
-				</div>
-
-			</div>
-			<!--end / INFO columna 2 -->
-
-			<!--INFO columna 3-->
-			<div id='info-col3' class="columnasInfo oculto">
-				<div class="item item-A  col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-					<p class="text ">3) Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.</p>
-					<p class="text ">El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.</p>
-				</div>
-
-				<div class="item item-B col-xs-12 col-sm-4 col-md-4 ol-lg-4 ">
-					<p class="text destacado text-uppercase">ARTÍCULOS PROMOCIONALES CON IDENTIFICACIÓN DE MARCA.</p>
-					<p class="text block">La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país. </p>
-					<p class="text ">La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.</p>
-					<p class="text block">Este programa te permitirá tener material promocional con:</p>
-					<ul class="block">
-						<li class="text-uppercase">
-							<span class="glyphicon glyphicon-chevron-right"></span>
-							El logo de tu empresa
-						</li>
-						<li class="text-uppercase">
-							<span class="glyphicon glyphicon-chevron-right"></span>
-							El logo de nufarm
-						</li>
-					</ul>
-				</div>
-
-				<div class="item item-C col-xs-12 col-sm-4 col-md-4 ol-lg-4">
-					<img class="imagen" src="assets/images/imagen-A.png" alt="">
-				</div>
-
-			</div>
-			<!--end / INFO columna 3 -->
-
-
-			<!--FOOTER-->
-			<div class="footer-menu col-xs-12 col-sm-12 col-md-12 ol-lg-12">
-				<div class="block-items">
-					<a href="#" >
-						<div class="item ">
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
-								<img class="imagen" src="assets/images/ICON-A.png" alt="">
-							</div>
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
-								<img class="flecha" src="assets/images/sub-flecha.png" alt="">
-							</div>
-							<hr>		
-							<p class="text-uppercase">Condiciones<br>generales</p>
-						</div>
-					</a>
-
-					<a href="#" >
-						<div class="item">
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
-								<img class="imagen" src="assets/images/ICON-B.png" alt="">
-							</div>
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
-								<img class="flecha" src="assets/images/sub-flecha.png" alt="">
-							</div>
-							<hr>		
-							<p class="text-uppercase">Blog</p>
-						</div>
-					</a>
-
-					<a href="#" >
-						<div class="item">
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
-								<img class="imagen" src="assets/images/ICON-C.png" alt="">
-							</div>
-							<div class=" col-xs-12 col-sm-12col-md-12 ol-lg-12">
-								<img class="flecha" src="assets/images/sub-flecha.png" alt="">
-							</div>
-							<hr>		
-							<p class="text-uppercase">Contacto</p>
-						</div>
-					</a>
-				</div>
-				
-			</div>
-			<!--end / FOOTER-->
-
-
-
+<div class="container-fluid">
+	<div class="col-xs-12 head">
+		<div class="inner">
+			<img src="assets/images/logo-white.png" id="Nufarm" title="Nufarm" alt="Imagen no encontrada">
+			<img src="assets/images/logo-green.png" id="Nufarm Gold" title="Nufarm Gold" alt="Imagen no encontrada">
 		</div>
-		<!--end / contenido-->
-
 	</div>
-	<!--end / base-->
+
+	<div class="col-xs-12 top-menu">
+		<div class="inner">
+			<a href="#">
+				condiciones comerciales
+			</a>
+			<a href="#">
+				contacto
+			</a>
+		</div>
+	</div>
+
+	<div class="inner">
+
+		<div class="col-xs-12 col-sm-4 login-box" id="box1">
+			<div class="icon">
+				<img src="assets/images/icon-a.png">
+			</div>
+			<h3>Marketing<br><span>Net</span></h3>
+			<p class="pre">
+				Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores...
+			</p>
+			<a href="#" class="show-more">
+				<span>más info</span>
+			</a>
+			<a href="#" class="goto">
+				ver catálogo
+			</a>
+		</div>
+
+		<div class="col-xs-12 col-sm-4 login-box" id="box2">
+			<div class="icon">
+				<img src="assets/images/icon-b.png">
+			</div>
+			<h3>vendedor<br><span>estrella</span></h3>
+			<p class="pre">
+				Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores...
+			</p>
+			<a href="#" class="show-more">
+				<span>más info</span>
+			</a>
+			<a href="#" class="goto">
+				ver estado
+			</a>
+		</div>
+
+		<div class="col-xs-12 col-sm-4 login-box" id="box3">
+			<div class="icon">
+				<img src="assets/images/icon-c.png">
+			</div>
+			<h3>Plan<br><span class="de"> de </span><span>Negocios</span></h3>
+			<p class="pre">
+				Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores...
+			</p>
+			<a href="#" class="show-more">
+				<span>más info</span>
+			</a>
+			<a href="#" class="goto">
+				ver estado
+			</a>
+		</div>
+
+		<div class="col-xs-12 info-box">
+			<div class="col-xs-4">
+				<p>
+					Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.
+					<br>
+					El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.
+					<br>
+					<br>
+					<span>
+						<i class="glyphicon glyphicon-chevron-right"></i> el logo de tu empresa
+					</span>
+					<span>
+						<i class="glyphicon glyphicon-chevron-right"></i> el logo de nufarm
+					</span>
+				</p>
+			</div>
+
+			<div class="col-xs-4">
+				<p>
+					<span>
+						ARTÍCULOS PROMOCIONALES<br>CON IDENTIFICACIÓN DE MARCA.
+					</span>
+					La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país.
+					<br>
+					La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.
+					<br>
+					<br>
+					Este programa te permitirá tener material promocional con:
+				</p>
+			</div>
+
+			<div class="col-xs-4">
+				<img class="img-responsive" src="assets/images/imagen-A.png">
+			</div>
+
+			<div class="col-xs-12">
+				<a href="#" class="goto">
+					ir a catálogo marketing net
+				</a>
+			</div>
+		</div>
+
+		<div class="col-xs-12 info-box">
+			<div class="col-xs-4">
+				<p>
+					Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.
+					<br>
+					El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.
+					<br>
+					<br>
+					<span>
+						<i class="glyphicon glyphicon-chevron-right"></i> el logo de tu empresa
+					</span>
+					<span>
+						<i class="glyphicon glyphicon-chevron-right"></i> el logo de nufarm
+					</span>
+				</p>
+			</div>
+
+			<div class="col-xs-4">
+				<p>
+					<span>
+						ARTÍCULOS PROMOCIONALES<br>CON IDENTIFICACIÓN DE MARCA.
+					</span>
+					La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país.
+					<br>
+					La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.
+					<br>
+					<br>
+					Este programa te permitirá tener material promocional con:
+				</p>
+			</div>
+
+			<div class="col-xs-4">
+				<img class="img-responsive" src="assets/images/imagen-A.png">
+			</div>
+
+			<div class="col-xs-12">
+				<a href="#" class="goto">
+					ver estado
+				</a>
+			</div>
+		</div>
+
+		<div class="col-xs-12 info-box">
+			<div class="col-xs-4">
+				<p>
+					Se trata de una iniciativa de cooperación integral, que Nufarm le brinda a un grupo de distribuidores seleccionados, con el fin de realizar acciones de marketing en conjunto.
+					<br>
+					El objetivo de este programa es generar un vínculo directo entre Nufarm y su red de distribuidores, unificando la comunicación y la aplicación la imagen corporativa.
+					<br>
+					<br>
+					<span>
+						<i class="glyphicon glyphicon-chevron-right"></i> el logo de tu empresa
+					</span>
+					<span>
+						<i class="glyphicon glyphicon-chevron-right"></i> el logo de nufarm
+					</span>
+				</p>
+			</div>
+
+			<div class="col-xs-4">
+				<p>
+					<span>
+						ARTÍCULOS PROMOCIONALES<br>CON IDENTIFICACIÓN DE MARCA.
+					</span>
+					La realización de artículos promocionales permite crear una buena identificación de marca a la vez de unificar la imagen de Nufarm a nivel país.
+					<br>
+					La compra centralizada permite lograr buenos costos y por sobre todo una calidad homogénea y asegurada.
+					<br>
+					<br>
+					Este programa te permitirá tener material promocional con:
+				</p>
+			</div>
+
+			<div class="col-xs-4">
+				<img class="img-responsive" src="assets/images/imagen-A.png">
+			</div>
+
+			<div class="col-xs-12">
+				<a href="#" class="goto">
+					ver estado
+				</a>
+			</div>
+		</div>
+	</div><!-- end inner -->
+</div>
 
 
 <?php include 'inc/footer.php' ?>
